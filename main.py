5a0d324115083448e2c4ee4337202a12a4f9f402
Paulina Żurek <paucik94@gmail.com>
2022-03-19 12:55:17 +0100

    Init class

---
+++ main.py
@@ -0,0 +1,7 @@
+class Card:
+   def __init__(self, name, last_name, company_name, job_position, email):
+       self.name = name
+       self.last_name = last_name
+       self.company_name = company_name
+       self.job_position = job_position
+       self.email = email
